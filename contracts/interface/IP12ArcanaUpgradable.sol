667519b8fa83c2a9053de2c75c7e8603fca9ad90
River <754865974@qq.com>
2022-09-20 17:41:16 +0800

    ✨ feat(verify.ts): action|verify script

--- contracts/interface/IP12ArcanaUpgradable.sol
+++ contracts/interface/IP12ArcanaUpgradable.sol
@@ -2,5 +2,5 @@
 pragma solidity ^0.8.0;
 
 interface IP12ArcanaUpgradable {
-  function getVotingPower(uint256 tokenId) view external returns (uint256);
+  function getVotingPower(uint256 tokenId) external view returns (uint256);
 }
